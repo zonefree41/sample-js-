6af10c36bfecf4d62119a9e6b2659f1d52b60642
zonefree41 <zonefree41@gmail.com>
2023-04-04 13:17:24 -0400

    1.3 JS exercise JS App

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ let pokemonList = [
 
   { name: "Charmander", height: 0.6, types: "water" },
 ];
+
+/* Above lists the pokemon inside an array titled "pokemonList". The array contains objects with
+characteristics for each pokemon.*/
+
+let pokemonBig = " - wow, that is a big pokemon.";
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height >= 1.7) {
+    document.write(
+      pokemonList[i].name +
+        " (height: " +
+        pokemonList[i].height +
+        ")" +
+        pokemonLarge +
+        " <br>"
+    );
+  } else {
+    document.write(
+      pokemonList[i].name + " (height: " + pokemonList[i].height + ")<br>"
+    );
+  }
+}
+
+/*this for loop has an if...else statement that identifies any pokemon greater than or equal to 1.7m.*/
