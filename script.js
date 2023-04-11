5e9f85f9e0e64d30e4b03c52e28e5e69910b4a89
zonefree41 <zonefree41@gmail.com>
2023-04-11 16:49:27 -0400

    pokemonRepository

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
-let pokemonList = [
-  { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+let pokemonRepository = (function () {
+  let pokemonList = [];
 
-  { name: "Ditto", height: 0.3, types: ["normal"] },
-
-  { name: "Charmander", height: 0.6, types: "water" },
-];
-
-/* Above lists the pokemon inside an array titled "pokemonList". The array contains objects with
-characteristics for each pokemon.*/
-
-let pokemonBig = " - wow, that is a big pokemon.";
-
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height >= 1.7) {
-    document.write(
-      pokemonList[i].name +
-        " (height: " +
-        pokemonList[i].height +
-        ")" +
-        pokemonBig +
-        " <br>"
-    );
-  } else {
-    document.write(
-      pokemonList[i].name + " (height: " + pokemonList[i].height + ")<br>"
-    );
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
-}
 
-/*this for loop has an if...else statement that identifies any pokemon greater than or equal to 1.7m.*/
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+
+console.log(pokemonRepository.getAll()); // []
+pokemonRepository.add({ name: "Pikachu" });
+console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
