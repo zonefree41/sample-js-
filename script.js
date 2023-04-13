4eea24c4286950f92b1b370472ea3b5dd917f043
zonefree41 <zonefree41@gmail.com>
2023-04-13 12:22:40 -0400

    updating pokemonRepository

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      Name: "Bulbasaur",
+      height: 0.7,
+      types: ["grass", "poison"],
+    },
+    {
+      Name: "Charizard",
+      height: 0.7,
+      types: ["fire", "flying"],
+    },
+  ];
+
+  function getAll() {
+    return pokemonList;
+  }
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    getAll: getAll,
+    add: add,
+  };
+})();
+
+console.log(pokemonRepository);
+console.log(pokemonRepository.getAll());
 pokemonRepository.getAll().forEach(function (pokemon) {
   if (pokemon.height > 2) {
     document.write(
       pokemon.name +
         " (height; " +
         pokemon.height +
-        " m) - Wow, that's big!" +
+        " m) - wow that's big!" +
         "<br>"
     );
   } else {
